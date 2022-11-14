84b583ff6476d4e29c41029693db681a46fb0275
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-11-14 02:39:51 -0300

    first commit

---
+++ database.js
@@ -0,0 +1,58 @@
+const mangas = [
+    {
+        id: 1,
+        name: 'Chainsawman',
+        author: 'Tatsuki Fujimoto',
+        volumes: 12,
+        genre: ['Action', 'Comedy', 'Horror', 'Drama'],
+        hasRead: true,
+        rocommended: true
+    },
+    {
+        id: 2,
+        name: 'Sayonara Eri',
+        author: 'Tatsuki Fujimoto',
+        volumes: 1,
+        genre: ['Mistery', 'Slice of Life', 'Drama'],
+        hasRead: false,
+        rocommended: false
+    },
+    {
+        id: 3,
+        name: 'Vagabond',
+        author: 'Inoue Takehiko',
+        volumes: 38,
+        genre: ['Action', 'Comedy', 'Historical', 'Drama'],
+        hasRead: true,
+        rocommended: true
+    },
+    {
+        id: 4,
+        name: 'Oyasumi Punpun',
+        author: 'Asano Inio',
+        volumes: 13,
+        genre: ['Drama', 'Romance', 'Mystery', 'Thriller'],
+        hasRead: true,
+        rocommended: true
+    },
+    {
+        id: 5,
+        name: 'Mirai Nikki',
+        author: 'Esuno Sakae',
+        volumes: 12,
+        genre: ['Action', 'Thriller', 'Psychological', 'Drama'],
+        hasRead: true,
+        rocommended: false
+    },
+    {
+        id: 6,
+        name: 'Shingeki no Kyojin',
+        author: 'Isayama Hajime',
+        volumes: 34,
+        genre: ['Action', 'Comedy', 'Horror', 'Drama'],
+        hasRead: true,
+        rocommended: false
+    }
+]
+
+module.exports = mangas;
